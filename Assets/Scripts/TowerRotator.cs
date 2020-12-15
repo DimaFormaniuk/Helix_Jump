00367c7a901bb4ad3fbb592e944a615d8768766b
DimaFormaniuk <44363635+DimaFormaniuk@users.noreply.github.com>
2020-12-15 17:07:16 +0200

    lesson 6

---
+++ Assets/Scripts/TowerRotator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class TowerRotator : MonoBehaviour
+{
+    [SerializeField] private bool _androidTouch;
+    [SerializeField] private float _rotateSpeed;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (_androidTouch)
+        {
+            AndroidTouch();
+        }
+        else
+        {
+            HandleKey();
+        }
+    }
+
+    private void AndroidTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                RotateTower(touch.deltaPosition.x);
+            }
+        }
+    }
+
+    private void HandleKey()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotateTower(1f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            RotateTower(-1f);
+        }
+    }
+
+    private void RotateTower(float x)
+    {
+        float torque = x * Time.deltaTime * _rotateSpeed;
+        _rigidbody.AddTorque(Vector3.up * torque);
+    }
+}
