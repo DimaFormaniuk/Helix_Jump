667a792075f7fd2b23965dfb14e1c5411c87e806
DimaFormaniuk <44363635+DimaFormaniuk@users.noreply.github.com>
2020-12-15 15:07:54 +0200

    lesson 4

--- Assets/Scripts/TowerBuilder.cs
+++ Assets/Scripts/TowerBuilder.cs
@@ -19,5 +19,18 @@ public class TowerBuilder : MonoBehaviour
     {
         GameObject beam = Instantiate(_beam, transform);
         beam.transform.localScale = new Vector3(1, _levelCount / 2f, 1);
+
+        Vector3 spawnPosition = beam.transform.position;
+        spawnPosition.y += beam.transform.localScale.y;
+        for (int i = 0; i < _levelCount; i++)
+        {
+            SpawnPlatform(_platform[Random.Range(0, _platform.Length)],ref spawnPosition, beam.transform);
+        }
+    }
+
+    private void SpawnPlatform(Platform platform, ref Vector3 spawnPosition, Transform parent)
+    {
+        Instantiate(platform, spawnPosition, Quaternion.Euler(0, Random.Range(0, Random.Range(0, 360)), 0), parent);
+        spawnPosition.y -= 1;
     }
 }
